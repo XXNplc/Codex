e20f3231e2ce0c1ee85ca5ba305d5c249893cd96
XXNplc <kalwixx@icloud.com>
2026-07-15 02:09:49 +0800

    Add luci-app-client-monitor overview include

---
+++ package/luci-app-client-monitor/htdocs/luci-static/resources/view/status/include/35_client_monitor.js
@@ -0,0 +1,8 @@
+'use strict';
+'require baseclass';
+'require client_monitor.common as common';
+
+return baseclass.extend(common.createPage({
+	title: '\u5728\u7ebf\u8bbe\u5907',
+	showHeader: false
+}));
