f56e5eedbc0225a163ce4a7385ed69bd93e4b3d4
XXNplc <kalwixx@icloud.com>
2026-07-18 01:12:59 +0800

    Delete package/luci-app-client-monitor directory

--- package/luci-app-client-monitor/htdocs/luci-static/resources/view/client_monitor/overview.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-'require view';
-'require client_monitor.common as common';
-
-return view.extend(common.createPage({
-	title: '\u5728\u7ebf\u8bbe\u5907'
-}));
--- package/luci-app-client-monitor/htdocs/luci-static/resources/view/status/include/35_client_monitor.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-'require baseclass';
-'require client_monitor.common as common';
-
-return baseclass.extend(common.createPage({
-	title: '\u5728\u7ebf\u8bbe\u5907',
-	showHeader: false
-}));
